aadc9698f93744fe3ac47c5ddece63d8316dad6e
MayankkShrivastavaa <mshri252@gmail.com>
2026-07-12 12:51:27 +0530

    updated UI & basic functioning

--- dictionary-app/src/components/Dictionary.jsx
+++ dictionary-app/src/components/Dictionary.jsx
@@ -1,10 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
 
-const Dictionary = ()=>{
-    return(
-        <div>
-            <h1>Dictionary App</h1>
+const Dictionary = () => {
+  const [word, setWord] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState("");
+  const [wordData, setWordData] = useState(null);
+
+  const searchWord = async () => {
+    try {
+      if (word.trim().length === 0) {
+        setErrors("Please Enter a Word...");
+        return;
+      }
+
+      setLoading(true);
+      setErrors("");
+      let res = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+      );
+      console.log(res);
+      
+    } catch (error) {
+      console.log(error);
+      setErrors("Word Not Found");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-linear-to-r from-indigo-100 via-sky-100 to-cyan-100 flex justify-center items-center p-5">
+      <div className="bg-white/50 w-full max-w-2xl rounded-3xl p-8 shadow-xl">
+        <h1 className="text-4xl text-center font-bold text-indigo-600 mb-8">
+          📖Dictionary App
+        </h1>
+
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="Enter any Word"
+            className="flex-1 border-2 border-indigo-300 rounded-xl px-4 py-3 outline-none focus:border-indigo-600"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <button
+            className="bg-indigo-600 pointer: cursor-pointer px-10 text-lg font-medium text-white rounded-xl hover:bg-indigo-700 "
+            onClick={searchWord}
+          >
+            Search
+          </button>
         </div>
-    )
-}
+
+        {loading && (
+          <h2 className="text-center text-lg mt-6 text-indigo-400 font-semibold animate-pulse">
+            Searching...
+          </h2>
+        )}
+
+        {errors && (
+          <h2 className="text-center mt-6 text-lg text-red-800 font-semibold">
+            {errors}
+          </h2>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Dictionary;
